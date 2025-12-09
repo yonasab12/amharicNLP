ca6ad33a5533b5b61b92ed61b343129335544712
Desalegn <4308161+DesBw@users.noreply.github.com>
2025-12-09 10:26:21 +0300

    Update and rename tokenizer.py to word_tokenizer.py
    I am adding sentence tokenizer. For that I am renaming it to word_tokenizer. 
    I also made improvements to handle rare cases of quotations.

--- amharicNLP/resources/word_tokenizer.py
+++ amharicNLP/resources/word_tokenizer.py
@@ -3,39 +3,35 @@ import re
 class AmharicWordTokenizer:
     """
     Improved Tokenizer for Amharic text.
-    Handles punctuation, opening/closing quotes, contractions, and parentheses/brackets.
+    Handles punctuation, opening/closing quotes, and parentheses/brackets.
     """
 
     # Opening quotes anywhere attached
     OPENING_QUOTES = [
-        (re.compile(r'([“"`«])([^\s])'), r'\1 \2'),  # space after the quote
+        (re.compile(r'([‹“"`«‛‘])(?!\s|[‹“"`«‛‘])'), r'\1 '),  # space after the quote
     ]
 
     # Closing quotes anywhere attached
     CLOSING_QUOTES = [
-        (re.compile(r'([^\s])([”"\’])'), r'\1 \2'),  # space before the quote
+        (re.compile(r'([^\s])([›”"»’])'), r'\1 \2'),  # space before the quote
     ]
-
     # Amharic punctuation marks
     PUNCTUATION = [
-        (re.compile(r'\.'), r' . '),
+        (re.compile(r"\'"), r" ' "),
+        (re.compile(r'"'), r' " '),
         (re.compile(r'፡'), r' ፡ '),
         (re.compile(r'[!?]'), r' \g<0> '),
         (re.compile(r'።'), r' ። '),
         (re.compile(r'፣'), r' ፣ '),
         (re.compile(r'፤'), r' ፤ '),
+        (re.compile(r'≈'), r' ≈ '),
+        (re.compile(r'$'), r' $ '),
         (re.compile(r'[-–—]+'), r' \g<0> '),
     ]
 
     # Parentheses and brackets
     PARENS_BRACKETS = [
         (re.compile(r'([\(\)\[\]\{\}])'), r' \1 ')
-    ]
-
-    # Contractions / attached prefixes
-    CONTRACTIONS = [
-        (re.compile(r'\b(የ|በ|ከ|ለ|ውስጥ|ላይ|በላይ)([^\s]+)\b'), r'\1 \2'),
-        (re.compile(r'\b(እኔ|አንቺ|እሱ|እሷ)([^\s]+)\b'), r'\1 \2'),
     ]
 
     def tokenize(self, text):
@@ -53,10 +49,6 @@ class AmharicWordTokenizer:
 
         # Replace parentheses/brackets
         for regexp, substitution in self.PARENS_BRACKETS:
-            text = regexp.sub(substitution, text)
-
-        # Split contractions and compound words
-        for regexp, substitution in self.CONTRACTIONS:
             text = regexp.sub(substitution, text)
 
         # Split on whitespace and return tokens
